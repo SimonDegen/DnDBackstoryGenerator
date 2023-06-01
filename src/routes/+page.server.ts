15c5edb0e05f42a7ac71bef28ec88cef47f25a7f
Simon Degen <mil-sideg@fiskeridir.no>
2023-06-01 13:25:59 +0200

    edited the nodejs version

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,7 +3,7 @@ import { openAi } from '../openAi';
 import type { CharacterResponse } from '../types/charResponse';
 
 export const config = {
-    runtime: 'nodejs18.x'
+    runtime: 'nodejs18.14.2'
   };
 
 /** @type {import('./$types').Actions} */
